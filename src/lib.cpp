102222d1fb2f8cb59c34075977b04bb8520c696b
niuchangsheng <changsheng.niu@gmail.com>
2025-12-20 21:50:18 +0800

    Replace src with minimal C++ KV implementation and update test

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,5 +1,23 @@
-#include "lib.h"
+#include "kv.h"
 
-int kv::add(int a, int b) {
-    return a + b;
+void KVStore::set(const std::string& key, const std::string& value) {
+    std::lock_guard<std::mutex> lg(mu_);
+    m_[key] = value;
+}
+
+std::optional<std::string> KVStore::get(const std::string& key) const {
+    std::lock_guard<std::mutex> lg(mu_);
+    auto it = m_.find(key);
+    if (it == m_.end()) return std::nullopt;
+    return it->second;
+}
+
+bool KVStore::del(const std::string& key) {
+    std::lock_guard<std::mutex> lg(mu_);
+    return m_.erase(key) > 0;
+}
+
+void KVStore::clear() {
+    std::lock_guard<std::mutex> lg(mu_);
+    m_.clear();
 }
